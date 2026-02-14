6ac64178d8eae7e03f77536f5c3ff5f5d6d285ab
Adrian-Cealic <adrian.cealic@gmail.com>
2026-02-14 19:47:08 +0200

    add unified courier create and drone service methods

--- src/services/courierService.ts
+++ src/services/courierService.ts
@@ -1,12 +1,14 @@
 import { api } from './api';
-import type { Courier, CreateBikeCourierRequest, CreateCarCourierRequest } from '../types';
+import type { Courier, CreateCourierRequest, CreateBikeCourierRequest, CreateCarCourierRequest, CreateDroneCourierRequest } from '../types';
 
 export const courierService = {
   getAll: () => api.get<Courier[]>('/couriers'),
   getById: (id: string) => api.get<Courier>(`/couriers/${id}`),
   getAvailable: () => api.get<Courier[]>('/couriers/available'),
   getAvailableForWeight: (weight: number) => api.get<Courier[]>(`/couriers/available/${weight}`),
+  create: (data: CreateCourierRequest) => api.post<Courier>('/couriers', data),
   createBike: (data: CreateBikeCourierRequest) => api.post<Courier>('/couriers/bike', data),
   createCar: (data: CreateCarCourierRequest) => api.post<Courier>('/couriers/car', data),
+  createDrone: (data: CreateDroneCourierRequest) => api.post<Courier>('/couriers/drone', data),
   delete: (id: string) => api.delete(`/couriers/${id}`),
 };
